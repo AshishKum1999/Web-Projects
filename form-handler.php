eb023ece9cdafede0c1bb752cd0b1ad2d39eaf7e
Ashish Kumar <ashish.48official@gmail.com>
2023-03-20 10:08:38 +0530

    My First Website

---
+++ form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$massage = $_POST['massage'];
+
+$email_from='';
+
+$email_subject = 'New From Submission';
+
+$email_body ="User Name : $name. \n". 
+             "User Email : $visitor_email. \n".
+              "User Subject : $subject. \n".
+               "User Massage : $massage. \n";
+
+$to = 'ashish.48official@gmail.com';
+$headers = "From: $email_from \r\n"
+$headers . = "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+header("Location: contact.html");
+
+
+?>
